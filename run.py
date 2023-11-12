d865ad15df3c84a49a8747d6c60227fb3f2344e7
HanByul Yang <hanbyul.yang@gmail.com>
2023-11-12 19:48:04 +0900

    log for starting gradio app.

--- run.py
+++ run.py
@@ -36,7 +36,7 @@ with gr.Blocks() as demo:
         "애리": "check_in_aeri",
         "지은": "check_in_jieun",
     }
-    
+
     button_dict = {}
     with gr.Row():
         for name, api_name in name_dict.items():
@@ -45,10 +45,10 @@ with gr.Blocks() as demo:
 
 
     excuse_radio = gr.Radio(["없음", "운동", "재택", "연차"], label="개인사유", info="다른 사정이라도 있으셨나요?")
-    
+
     with gr.Column():
         custom_checker = gr.Checkbox(label="네, 시간을 직접 넣을게요.", info="출근시간이 너무 지났으면 직접 넣으실 수 있어요.")
-        with gr.Row():       
+        with gr.Row():
             custom_hour = gr.Number(label="HH", minimum= 0, maximum=24)
             custom_minute = gr.Number(label="MM", minimum= 0, maximum=60)
     test_result = gr.Textbox(label="", placeholder= "Welcome!")
@@ -63,4 +63,5 @@ with gr.Blocks() as demo:
             outputs=[test_result],
             api_name=api_name)
 
+    logger.info("Launching...")
     demo.launch(server_name="0.0.0.0", auth=(constants.USER_ID, constants.PASSWORD))
