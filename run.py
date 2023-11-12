aa35232b0a4d9f3d7d8ea5724e237025d0de076a
Yujin Lee <yujin.gha@gmail.com>
2023-11-12 17:32:26 +0900

    Add column between checkbox and custom timbox for mobile

--- run.py
+++ run.py
@@ -45,11 +45,12 @@ with gr.Blocks() as demo:
 
 
     excuse_radio = gr.Radio(["없음", "운동", "재택", "연차"], label="개인사유", info="다른 사정이라도 있으셨나요?")
-
-    with gr.Row():
+    
+    with gr.Column():
         custom_checker = gr.Checkbox(label="네, 시간을 직접 넣을게요.", info="출근시간이 너무 지났으면 직접 넣으실 수 있어요.")
-        custom_hour = gr.Number(label="HH", minimum= 0, maximum=24)
-        custom_minute = gr.Number(label="MM", minimum= 0, maximum=60)
+        with gr.Row():       
+            custom_hour = gr.Number(label="HH", minimum= 0, maximum=24)
+            custom_minute = gr.Number(label="MM", minimum= 0, maximum=60)
     test_result = gr.Textbox(label="", placeholder= "Welcome!")
 
     for api_name, btn in button_dict.items():
